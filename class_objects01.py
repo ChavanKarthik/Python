43842b7e34d1dc5c9f3bfe25702339a5a345023c
karthikbeehyv <karthik@beehyv.com>
2019-03-22 15:49:29 +0530

    Testing commit and push functionality

--- class_objects01.py
+++ class_objects01.py
@@ -1,31 +1,29 @@
 class Computer:
 
-	def __init__(self,name,age,height):
-		self.name=name
-		self.age=age
-		self.height=height
+	def __init__(self, name, age, height):
+		self.name = name
+		self.age = age
+		self.height = height
+		print("Name   : {}\nAge    : {}\nHeight : {}".format(self.name, self.age, self.height))
 
-	def display(self):
-		print("Name   : %s \nAge    : %i \nHeight : %i"%(self.name,self.age,self.height)) 		
+obj1 = Computer('sai', 25, 5)
+obj2 = Computer('ram', 65, 6)
 
-obj1 = Computer('sai',25,5)
-obj2 = Computer('ram',65,6)
-
-obj1.display()
-obj2.display()
+# obj1.display()
+# obj2.display2()
 
 class Car:
 
 	def __init__(self):
 
 		self.brand = 'Skoda'
-		self.model='Laura'
+		self.model = 'Laura'
 
 	def update(self):
-		self.model='Laura Elegance'
+		self.model = 'Laura Elegance'
 
-c1=Car()
-c2=Car()
+c1 = Car()
+c2 = Car()
 
 c2.name='Rapid'
 c2.model='High_End'
@@ -33,8 +31,8 @@ c2.model='High_End'
 c1.update()
 c2.update()
 
-print(c1.brand, c1.model)
-print(c2.brand, c2.model)
+#print(c1.brand, c1.model)
+#print(c2.brand, c2.model)
 
 class School:
 	def __init__(self):
@@ -44,4 +42,4 @@ class School:
 sch1=School()
 sch2=School()
 
-print(sch1.marks,sch1.rank)
+#print(sch1.marks,sch1.rank)
