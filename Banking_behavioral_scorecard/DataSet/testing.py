808e6c3104b496a58355942ea85249b954c64ff5
karthikbeehyv <karthik@beehyv.com>
2019-09-30 12:24:56 +0530

    Implemented multiple algorithms and refactored the existing code.

--- Banking_behavioral_scorecard/DataSet/testing.py
+++ Banking_behavioral_scorecard/DataSet/testing.py
@@ -5,9 +5,19 @@ import pandas as pd
 df = pd.DataFrame([[6, 2, 6, 0],
                    [3, 4, 9, 1],
                    [6, 6, 4, 5],
-                   [6, 3, 8, 4]],
+                   [1, 1, 5, 5]],
                   columns=list('ABCD'))
 
-print(df)
-print(df.iloc[:, 2:3])
+# df['new_col'] = 5
+# test = df['new_col']
 
+# df.insert(1, 'Col2', 999)
+
+print(df.columns)
+
+print(df.iloc[:, -1:])
+# df = df.iloc[:, 1:-1]
+# print(df.iloc[:, 2:3].columns())
+# columns = df.columns.values.tolist()
+# training_df = df[df['A'] != 3]
+# print(df)
